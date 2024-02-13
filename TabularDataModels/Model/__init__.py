2bd3a9af6b5f42faabd42a11f39f76d6e81f1df5
VRM1 <vineethrakesh@gmail.com>
2024-02-12 19:10:17 -0800

    moved deeptabular to its own repo

--- TabularDataModels/Model/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('/home/vineeth/Documents/GitWorkSpace/PytorchRecipes/')
-sys.path.append('/home/vineeth/Documents/GitWorkSpace/PytorchRecipes/TabularDataModels')
-from .MLP import Mlp
-from .TabMLP import TabMLP
-from .TabResnet import TResnet
-from .FTTransformer import FTransformer
-from .Convolution import Conv
-from .SoftOrderingCNN import SoftOrdCNN
